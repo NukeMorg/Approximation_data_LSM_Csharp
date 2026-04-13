8d0fc34ec2653b0ccd68de2faa7a0d929d315ae5
Nikita Morgavchuk <workmorgnik@gmail.com>
2026-04-13 23:17:05 +0300

    Remote AI text

---
+++ MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CurseWork
+{
+    public class MainViewModel
+    {
+        public double MSE { get; set; }
+        public double RMSE { get; set; }
+        public double R2 { get; set; }
+        public double AdjustedR2 { get; set; }
+
+        public MainViewModel()
+        {
+            MSE = 0;
+            RMSE = 0;
+            R2 = 0;
+            AdjustedR2 = 0;
+        }
+    }
+}
